257eb4fea6c2c2629fb6bb0955351a6303cf0658
tokitsu <toki2222son@yahoo.co.jp>
2021-07-01 09:31:46 +0900

    add signin signup function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::API
   rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
   
   private
+
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
   
   def not_authorized
     render json: { error: 'Not authorized' }, status: :unauthorized
